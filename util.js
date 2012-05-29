729588f8941a55c51c83144a690e7bf27191a50a
sam <sam@green-island.(none)>
2012-05-29 14:48:36 -0400

    Add comments and rename some variables.

--- util.js
+++ util.js
@@ -1,11 +1,12 @@
-window.onload = init;
-
 /* Globals */
 var map;
-var currentPos;
+var currentLocation;
 var destination;
 
-function init() {
+/* Determine if this browser implements geolocation. If so, go ahead and
+ * create a map centerd on the current location.
+ */
+function initialize() {
 
   var geolocationOptions = {
     enableHighAccuracy: true,
@@ -13,75 +14,85 @@ function init() {
   };
   
   if (navigator.geolocation) {
-    var addressConverter = new google.maps.Geocoder();
-    addressConverter.geocode({address:"30 applegate lane, providence, ri"}, geocodeHandler);
-
-    /*navigator.geolocation.getCurrentPosition(createMap, 
-                                             geolocationErrorHandler, 
-                                             geolocationOptions); */
+    navigator.geolocation.getCurrentPosition(createMap, geolocationErrorHandler, geolocationOptions); 
   }
   else {
     alert("I'm sorry, but for this application to work it will need an updated browser." +
-          "\n\nTry again with the latest version of Google Chrome.");
+          "\n\nThis app is sure to work with the latest version of Chrome.");
   }
 }
 
-function geocodeHandler(geocoderResults, geocoderStatus) {
-
-  if (geocoderStatus == google.maps.GeocoderStatus.OK) {
-    destination = geocoderResults[0].geometry.location;
-    map = new google.maps.Map(document.getElementById('map_canvas'), {center: destination, zoom: 18, mapTypeId: google.maps.MapTypeId.ROADMAP});
-  }
-  else {
-    alert("Geocoder failed for the following reason: " + geocoderStatus);
-  }
-}
-
-
-
-
+/* Draws a map centered on on the given position.
+ *
+ * input: position A Position object as defined by W3C's geolocation API.
+ */
 function createMap(position) {
 
+  currentLocation = position2LatLng(position);
+
   var mapOptions = {
-    center: new google.maps.LatLng(position.coords.latitude, 
-                                   position.coords.longitude),
+    center: currentLocation,
     zoom: 18,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
   map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 
-}
-
-/*
-function makeMap(position) {
-
-  // position.coords.accuracy;
-
-  var mapOptions = {
-    center: new google.maps.LatLng(position.coords.latitude, 
-                                   position.coords.longitude),
-    zoom: 18,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-
-  var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-
+  //temporary hack to place a marker at currenLocation. Delete once testing is done.
   var markerOptions = {
     animation: google.maps.Animation.DROP,
     map: map,
-    position: new google.maps.LatLng(position.coords.latitude,
-                                     position.coords.longitude),
+    position: currentLocation,
     title: "Current position",
     visible: true
   }
+  var currenPositionMarker = new google.maps.Marker(markerOptions);
 
-  var currentPositionMarker = new google.maps.Marker(markerOptions);
 }
 
-*/
+/* Convert the user's destination address to latitude and longitude via maps.google.Geocoder. */
+function convert2LatLng() {
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode({address: document.getElementById("destinationAddress").value}, geocodeHandler);
+}
 
-/* Message explaining why the application has not started. */
+/* Center the map on the destination coordinates
+ *
+ * input geocoderResults The result from calling maps.google.Geocoder.geocode().
+ * input geocoderStatus  Indicates whether the call was a success or resulted in error.
+ */
+function geocodeHandler(geocoderResults, geocoderStatus) {
+  if (geocoderStatus == google.maps.GeocoderStatus.OK) {
+    destination = geocoderResults[0].geometry.location;
+    map.setCenter(destination);
+
+    //temporary hack to place a marker at destination. Deleted once testing is done.
+    var markerOptions = {
+      animation: google.maps.Animation.DROP,
+      map: map,
+      position: destination,
+      title: "Destination",
+      visible: true
+    }
+    var currenPositionMarker = new google.maps.Marker(markerOptions);
+  }
+  else {
+    alert("Geocoder failed for the following reason: " + geocoderStatus);
+  }
+}
+
+/* Converts a Position object into a google.maps.LatLng object 
+ *
+ * input: posittion The Position object returned by the browser goelocation service.
+ */
+function position2LatLng(position) {
+  return new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+}
+
+/* Message explaining why the application has not started. 
+ * 
+ * input: error The error returned by the browser's Geolocation service
+ */
 function geolocationErrorHandler(error) {
 
   var errorMsg;
@@ -102,3 +113,6 @@ function geolocationErrorHandler(error) {
 
   alert (errorMsg + " Reload the page to try again.");
 }
+
+window.onload = initialize;
+
