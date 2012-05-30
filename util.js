276dd9c063bf7eafbe97f4e8dfb3f6a7d954c428
sam <sam@green-island.(none)>
2012-05-29 23:54:38 -0400

    Update destination whenever user puts in a new address.
    Also, changed the how the destination and the currentLocation
    global variables so that instead of referencing latLng objects,
    they now reference Marker objects.

--- util.js
+++ util.js
@@ -1,7 +1,28 @@
 /* Globals */
-var map;             // google.maps.Map object
-var currentPosition; // google.maps.latLng object
-var destination;     // google.maps.latLng object
+var map; // Reference our google.maps.Map object
+
+var currentPosition = new google.maps.Marker({
+  animation: google.maps.Animation.DROP,
+  title: "Current position",
+  visible: true
+}); 
+
+var destination = new google.maps.Marker({
+  icon: "stop.png",
+  animation: google.maps.Animation.DROP,
+  title: "Destination",
+  visible: true
+});     
+
+var circle = new google.maps.Circle({
+  clickable: false,
+  fillColor: "#FF0000",
+  fillOpacity: 0.35,
+  radius: 804.67200, // This is the meter equivalent of 1/2 a mile.
+  strokeColor: "#FF0000",
+  strokeOpacity: 0.8,
+  strokeWeight: 2
+});
 
 /* Determine if this browser implements geolocation. If so, go ahead and
  * create a map centerd on the current location.
@@ -24,86 +45,56 @@ function initialize() {
 
 /* Draws a map centered on on the given position.
  *
- * input: position A Position object as defined by W3C's geolocation API.
+ * parameter: position A Position object as defined by W3C's geolocation API.
  */
 function createMap(position) {
 
-  currentPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
   var mapOptions = {
-    center: currentPosition,
+    center: latLng,
     zoom: 15,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
   map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 
-  //temporary hack to place a marker at currenLocation. Delete once testing is done.
-  var markerOptions = {
-    animation: google.maps.Animation.DROP,
-    map: map,
-    position: currentPosition,
-    title: "Current position",
-    visible: true
-  }
-  var currenPositionMarker = new google.maps.Marker(markerOptions);
+  currentPosition.setPosition(latLng);
+  currentPosition.setMap(map);
 }
 
-/* Convert the user's destination address to latitude and longitude via the Geocoder object. */
+/* Determine the destination's coordinates then draw it on the map. 
+ */
 function convert2LatLng() {
   var geocoder = new google.maps.Geocoder();
-  geocoder.geocode({address: document.getElementById("destinationAddress").value}, setDestination);
+  geocoder.geocode({address: document.getElementById("destinationAddress").value}, drawDestination);
 }
 
 /* Center the map on the destination coordinates
  *
- * input results The result from calling maps.google.Geocoder.geocode().
- * input status  Indicates whether the call was a success or resulted in error.
+ * parameter: results The result from calling maps.google.Geocoder.geocode().
+ * parameter: status  Indicates whether the call was a success or resulted in error.
  */
-function setDestination(results, status) {
+function drawDestination(results, status) {
   if (status == google.maps.GeocoderStatus.OK) {
-    destination = results[0].geometry.location;
-    map.setCenter(destination);
+    latLng = results[0].geometry.location;
+    map.setCenter(latLng);
 
-    //temporary hack to place a marker at destination. Deleted once testing is done.
-    var markerOptions = {
-      icon: "stop.png",
-      animation: google.maps.Animation.DROP,
-      map: map,
-      position: destination,
-      title: "Destination",
-      visible: true
-    }
-    var currenPositionMarker = new google.maps.Marker(markerOptions);
+    destination.setPosition(latLng);
+    destination.setMap(map);
 
-    drawCircle(destination);
+    circle.setCenter(latLng);
+    circle.setMap(map);
+
   }
   else {
     alert("Geocoder failed for the following reason: " + status);
   }
 }
 
-// takes as input a google.maps.latLng object
-function drawCircle(latlng) {
-  var circleOptions = {
-    center: latlng,
-    clickable: false,
-    fillColor: "#FF0000",
-    fillOpacity: 0.35,
-    map: map,
-    radius: 804.67200,
-    strokeColor: "#FF0000",
-    strokeOpacity: 0.8,
-    strokeWeight: 2
-  }
-  var circle = new google.maps.Circle(circleOptions);
-}
-
-
-
 /* Message explaining why the application has not started. 
  * 
- * input: error The error returned by the browser's Geolocation service
+ * parameter: error The error returned by the browser's Geolocation service
  */
 function geolocationErrorHandler(error) {
 
